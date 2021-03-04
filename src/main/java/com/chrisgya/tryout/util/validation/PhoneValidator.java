870cba4d7636c17e9299177076666b140d8138e4
christian.gyaban <christian.gyaban@interswitchgroup.com>
2021-03-04 16:30:10 +0100

    updated phone number validation

--- src/main/java/com/chrisgya/tryout/util/validation/PhoneValidator.java
+++ src/main/java/com/chrisgya/tryout/util/validation/PhoneValidator.java
@@ -1,8 +1,6 @@
 package com.chrisgya.tryout.util.validation;
 
-import com.google.i18n.phonenumbers.NumberParseException;
-import com.google.i18n.phonenumbers.PhoneNumberUtil;
-import com.google.i18n.phonenumbers.Phonenumber;
+import com.chrisgya.tryout.util.PhoneUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
@@ -12,24 +10,8 @@ import javax.validation.ConstraintValidatorContext;
 @Slf4j
 public class PhoneValidator implements ConstraintValidator<Phone, String> {
 
-    PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
-
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
-        if (StringUtils.isBlank(value)) {
-            return true;
-        }
-
-        try {
-            var region = "NG";
-            if (value.startsWith("+")) {
-                region = phoneUtil.getRegionCodeForCountryCode(Integer.parseInt(value.substring(1, 3)));
-            }
-            Phonenumber.PhoneNumber phoneNumber = phoneUtil.parse(value, region);
-            return phoneUtil.isValidNumber(phoneNumber);
-        } catch (NumberParseException e) {
-            log.error("NumberParseException was thrown: {}", e);
-            return false;
-        }
+        return StringUtils.isBlank(value) || PhoneUtil.isValid(value);
     }
 }
